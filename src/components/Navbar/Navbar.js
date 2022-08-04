444a4421db94f11c2bc5caed0bc4ae46294f5e02
Sheersh Jindal <sheershjindal@gmail.com>
2022-08-04 07:52:54 +0530

    Initial Commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Nav } from './NavbarElements.js'
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        Nav
+      </Nav>
+    </>
+  )
+}
+
+export default Navbar
